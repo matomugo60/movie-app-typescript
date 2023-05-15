288e6a89d2f4131100b15666a97c599115ee34e1
matomugo60 <matomugo60@gmail.com>
2023-05-15 12:44:19 +0300

    fixed errors in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home/Home';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Route,
+// } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Detail from './components/Detail/Detail';
 
 function App() {
   return (
     <div className="">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home></Home>
+      <BrowserRouter>
+      <Home></Home>
+      <Detail/>
+      <Routes>
+          <Route path="/">
+            
           </Route>
           <Route path="/detail/:id">
-            <Detail/>
+            
           </Route>
-        </Switch>
-      </Router>
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../Header/Header';
+// import Header from '../Header/Header';
 import MoviesList from '../MoviesList/MoviesList';
 
 const Home = () => {
